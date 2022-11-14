661393d3f933d67087284bfc02f9c3110ffc70dd
yud11z <yudipro91@gmail.com>
2022-11-14 18:59:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    screens: {
+      'mobile': {'max': '375px'},
+      'destkop': '1366px',
+    },
+    extend: {
+      colors: {
+        pink: 'hsl(var(--color-pink) / <alpha-value>)',
+        lightpink: 'hsl(var(--color-lightpink) / <alpha-value>)',
+        lightred: 'hsl(var(--color-lightred) / <alpha-value>)',
+        verydarkcyan:  'hsl(var(--color-verydarkcyan) / <alpha-value>)',
+        verypaleblue: 'hsl(var(--color-verypaleblue) / <alpha-value>)',
+      },
+
+      fontFamily: {
+        poppins: ['Poppins'],
+        opensans: ['Open Sans'],
+      }
+    },
+  },
+  plugins: [],
+}
